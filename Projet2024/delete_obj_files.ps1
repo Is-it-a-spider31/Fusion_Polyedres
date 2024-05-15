1509469f6259d3b69f5c8a1c3b92ebca5ba401ae
Is it a spider? <jdubois10.pro@gmail.com>
2024-05-15 09:53:13 +0200

    Added a script to automatically delete the generated .obj file when running the program

---
+++ Projet2024/delete_obj_files.ps1
@@ -0,0 +1,7 @@
+
+# SCRIPT POWERSHELL POUR WINDOWS
+
+# Supprime les fichiers .obj qui sont generes par le projet cpp
+# (evite de supprimer les fichiers a la main avant chaque execution)
+$directory = "MergeTest\generated"  # Chemin du repertoire
+Get-ChildItem $directory -Filter *.obj | Remove-Item -Force
---
+++ Projet2024/delete_obj_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# SCRIPT BASH POUR LINUX
+
+# Supprime les fichiers .obj qui sont generes par le projet cpp
+# (evite de supprimer les fichiers a la main avant chaque execution)
+directory="MergeTest\generated"  # Chemin du repertoire
+find "$directory" -type f -name "*.obj" -delete
