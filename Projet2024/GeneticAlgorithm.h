2fbcc8713109662c24da01e4a2de1aedabb7b0fa
antoine <45201897+ISheepFr@users.noreply.github.com>
2024-05-15 22:01:10 +0200

    Début de structure pour algo génetique

---
+++ Projet2024/GeneticAlgorithm.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "Algorithm.h"
+#include <vector>
+#include <iostream>
+
+class GeneticAlgorithm : public Algorithm
+{
+
+public:
+	GeneticAlgorithm(const string& filename, int popSize, double probaCross, double probaMut, int maxIter);
+	void run() override;
+
+	//Getters
+	int getPopSize() const;
+	int getMaxIteration() const;
+	double getProbaCross() const;
+	double getProbaMut() const;
+
+	
+
+	// FONCTIONS TYPIQUE DU GA
+
+	//reséparer en , création, initialisation pop, initialisation, tournoi = selection, 
+	// croisements, mutation, fonction de check de critere d'arret
+
+	void createPop();
+	void initializePop();
+	
+	//renvoie les 2 winners
+	 vector<int>* turnament();
+
+	 void crossover();
+	 void mutation();
+
+	 //surement mettre en private ses trucs la
+
+	 void onePointCrossover(int first_parent, int sec_parent);
+	 void oneGeneMutation(int child);
+
+
+
+
+private:
+
+	int d_popSize;
+	int d_maxIteration;
+	
+	double d_crossoverProba;
+	double d_mutationProba;
+
+	vector<vector<int>> d_popActual;
+	vector<vector<int>> d_popNew;
+
+	vector<int> turnamentWinner;
+
+};
+
